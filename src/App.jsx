cd10d4788febc3071529d6fda41fc4b09a080878
dgaye <damegaye1887@gmail.com>
2023-10-26 00:39:25 +0000

    working on folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+
+import { NavBar, SearchFeed, VideoDetail, ChannelDetail, Feed } from './components'
+
+import { Box } from '@mui/material'
+import './App.css'
+
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  <BrowserRouter>
+  <Box>
+    <NavBar/>
+    <Routes>
+      <Route path="/" exact element={ <Feed/> }/>
+      <Route path="shearch/:searchInput" element={ <SearchFeed/> } />
+      <Route path="video/:id" element={ <VideoDetail/> }/>
+      <Route path="/channel/:id" element={ <ChannelDetail/> }/>
+
+    </Routes>
+  </Box>
+  </BrowserRouter>
 }
 
 export default App
---
+++ src/components/import.js
@@ -0,0 +1,6 @@
+export {default as NavBar} from "./NavBar"
+export {default as Feed} from "./Feed"
+export {default as VideoDetail} from "./VideoDetail"
+export {default as SearchFeed} from "./SearchFeed"
+export {default as ChannelDetail} from "./ChannelDetail"
+
