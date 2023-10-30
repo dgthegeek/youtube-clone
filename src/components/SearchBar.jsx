6936c70562cbaccf369b1c4646aa39eef6d97810
dgthegeek <damegaye1887@gmail.com>
2023-10-30 17:11:16 +0000

    fixed the useNavigate not working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,17 +7,21 @@ import { useNavigate } from 'react-router-dom'
 const SearchBar = () => {
     const [searchInput, setsearchInput] = useState('')
     const navigate = useNavigate()
+    console.log(searchInput)
     const handleSubmit = (e) => {
+        console.log(navigate);
         e.preventDefault();
+        e.stopPropagation()
         if (searchInput) {
+            console.log('ok');
             navigate(`/search/${searchInput}`)
         }
         setsearchInput('')
     }
 
     return (
-        <Paper component='form'sx={{ borderRadius: 20, border: '1px solid black', pl: 2, mr: { sm: 5 } }}>
-            <input type="text" className='search-bar' placeholder='Search...' value={searchInput} onSubmit={handleSubmit} onChange={(e) => { setsearchInput(e.target.value) }} />
+        <Paper onSubmit={handleSubmit} component='form' sx={{ borderRadius: 20, border: '1px solid black', pl: 2, mr: { sm: 5 } }}>
+            <input type="text" className='search-bar' placeholder='Search...' value={searchInput}  onChange={(e) => { setsearchInput(e.target.value) }} />
             <IconButton type='submit' sx={{ p: '10px' }}>
                 <Search />
             </IconButton>
