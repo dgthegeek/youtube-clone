762aeeb9f001343cf7f5d70e7e63ba021bf06b22
dgthegeek <damegaye1887@gmail.com>
2023-10-31 14:44:03 +0000

    implementing the video detail

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+
+import Videos from "./Videos";
+import { FetchData } from "../utils/API";
 
 const VideoDetail = () => {
-    return(
-        <div>VideoDetail</div>
-    )
-}
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
 
-export default VideoDetail
+  useEffect(() => {
+    FetchData(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items))
+
+  }, [id]);
+
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {videoDetail?.snippet?.title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
+              <Link to={`/channel/${videoDetail?.snippet?.channelId}`}>
+                <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
+                  {videoDetail?.snippet?.channelTitle}
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(videoDetail?.statistics?.viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(videoDetail?.snippet?.likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        </Box>
+        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
+          {/* <Videos videos={videos} direction="column" />  I will put the videos here */} 
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
